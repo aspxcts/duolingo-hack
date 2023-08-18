1a5aa01dffea8fc0d723e0955eda1e614228eefc
azamsafir <azamsafir@gmail.com>
2023-08-17 22:03:21 -0500

    began work on the fill in the blanks

--- QFillintheblanks.py
+++ QFillintheblanks.py
@@ -8,15 +8,16 @@ import sys
 import time
 
 reader = easyocr.Reader(["en", "es"])
-mon = {'left': 808, 'top': 428, 'width': 590, 'height': 150}
+mon = {'left': 0, 'top': 0, 'width': 1920, 'height': 1080}
 
-def fillintheblanks():
+def fillintheblanksmethod():
     with mss.mss() as sct:
         while True:
             url = "https://api.mymemory.translated.net/get?q="
             sep = ''
             im = numpy.asarray(sct.grab(mon))
             readerresult = reader.readtext(im, detail=0)
+            print(readerresult)
             texttotranslate = sep.join(readerresult)
             from main import mainscript
             mainscript()
