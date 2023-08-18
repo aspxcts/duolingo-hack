2874f7bcd75d0d4fd79f2c90dd9d91feafda9af9
azamsafir <azamsafir@gmail.com>
2023-08-18 17:25:47 -0500

    50% done very broken

--- QFillintheblanks.py
+++ QFillintheblanks.py
@@ -8,7 +8,37 @@ import sys
 import time
 
 reader = easyocr.Reader(["en", "es"])
-mon = {'left': 0, 'top': 0, 'width': 1920, 'height': 1080}
+mon = {'top': 370, 'left': 629, 'width': 600, 'height': 150}
+
+
+def createsentence():
+    with mss.mss() as sct:
+        mon2 = {'top': 500, 'left': 854, 'width': 300, 'height': 500}
+        while True:
+            url = "https://api.mymemory.translated.net/get?q="
+            sep = ''
+            sepspace = ' '
+            im = numpy.asarray(sct.grab(mon))
+            sentence = reader.readtext(im, detail=0)
+            print(sentence)
+            stringsentence = sep.join(sentence)
+            print(stringsentence)
+            im2 = numpy.asarray(sct.grab(mon2))
+            options = reader.readtext(im2, detail=0)
+            print(options)
+            stringoptions = sepspace.join(options)
+            print(stringoptions)
+
+            for option in options:
+                fullsentence = ''
+                fullsentence += stringsentence
+                fullsentence += ' '
+                fullsentence += option
+                print(fullsentence)
+
+
+            # texttotranslate = sep.join(readerresult)
+
 
 def fillintheblanksmethod():
     with mss.mss() as sct:
@@ -16,8 +46,8 @@ def fillintheblanksmethod():
             url = "https://api.mymemory.translated.net/get?q="
             sep = ''
             im = numpy.asarray(sct.grab(mon))
-            readerresult = reader.readtext(im, detail=0)
-            print(readerresult)
-            texttotranslate = sep.join(readerresult)
-            from main import mainscript
-            mainscript()
+            sentence = reader.readtext(im, detail=0)
+            # print(sentence)
+            createsentence()
+
+            # texttotranslate = sep.join(readerresult)
