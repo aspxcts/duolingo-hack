db5b49e8511a38871eb3d79d55da31a53ce643cf
azamsafir <azamsafir@gmail.com>
2023-08-18 23:19:25 -0500

    completely updated to handle 80% of questions

--- main.py
+++ main.py
@@ -2,85 +2,30 @@ import easyocr
 import time
 import mss
 import numpy
-import requests
-import json
-import tkinter
+import openai
+
+with open('C:\APIKEYS\openai.txt') as f:
+    key = f.read()
+with open('C:\ENDPOINTS\openai.txt') as f:
+    endpoint = f.read()
+
+openai.api_base = endpoint
+openai.api_key = key
+
 from QListenHear import listenandhear
 from QTypeES import typeinspanish
-from QFillintheblanks import fillintheblanks
+from QFillintheblanks import fillintheblanksmethod
 from QTypeEN import typeinenglish
 from QHowToSay import howtosayword
 from QWhichOne import whichone
 
-import cv2
-import pyautogui
-
-# pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-
-duolingo_spanish_words = (
-    "este, yo, tú, él, ella, usted, nosotros, vosotros, ellos, ellas, ustedes, "
-    "ser, estar, tener, hacer, ir, venir, decir, ver, comer, beber, dormir, "
-    "leer, escribir, hablar, escuchar, entender, aprender, querer, necesitar, "
-    "poder, saber, gustar, conocer, amar, oír, vivir, trabajar, estudiar, "
-    "jugar, comprar, vender, pagar, pedir, responder, preguntar, llegar, salir, "
-    "entrar, seguir, dejar, pensar, sentir, llevar, traer, empezar, terminar, "
-    "mirar, encontrar, perder, ayudar, buscar, esperar, usar, probar, olvidar, "
-    "recordar, explicar, caminar, correr, saltar, nadar, viajar, volar, conducir, "
-    "montar, subir, bajar, cruzar, recoger, organizar, limpiar, cocinar, compartir, "
-    "divertirse, bailar, cantar, reír, llorar, ganar, perder, competir, jugar, "
-    "practicar, ejercitar, descansar, mirar, ver, escuchar, oír, leer, escribir, "
-    "hablar, conversar, entender, aprender, enseñar, creer, opinar, soñar, desear, "
-    "esperar, necesitar, querer, amar, gustar, preferir, saber, conocer, estar, "
-    "sentir, ser, tener, haber, hacer, decir, ver, dar, ir, venir, saber, poder, "
-    "poner, salir"
-)
-
-duolingo_spanish_words2 = (
-    "dónde maleta yo dos este hermanas tú él ella usted nosotros vosotros ellos ellas ustedes "
-    "ser estar tener hacer ir venir decir ver comer beber dormir "
-    "leer escribir hablar escuchar entender aprender querer necesitar "
-    "poder saber gustar conocer amar oír vivir trabajar estudiar "
-    "jugar comprar vender pagar pedir responder preguntar llegar salir "
-    "entrar seguir dejar pensar sentir llevar traer empezar terminar "
-    "mirar encontrar pasaporte perder ayudar buscar esperar usar probar olvidar "
-    "recordar explicar caminar correr saltar nadar viajar volar conducir "
-    "montar subir bajar cruzar recoger organizar limpiar cocinar compartir "
-    "divertirse bailar cantar reír llorar ganar perder competir practicar "
-    "ejercitar descansar creer opinar soñar desear preferir conocer "
-    "sentir tener haber hacer poder dar estar ir decir contar caer "
-    "nacer morir matar gustar molestar enamorar preocupar gustar "
-    "parecer costar tocar abrazar besar escribir coger solucionar "
-    "resolver despedir saludar esperar alquilar comprar cerrar "
-    "abrir respetar acostar enfrentar enseñar aprender llamar gritar "
-    "hablar ver jugar bailar estudiar trabajar comer beber vivir "
-    "viajar cocinar dormir conocer escuchar gustar pensar hacer "
-    "necesitar querer tener ser estar ir venir decir ver dar "
-    "saber poder poner salir buscar encontrar pedir llevar traer "
-    "esperar entrar seguir dejar oír escribir creer recibir "
-    "sentir hacer conocer dejar amar mirar cambiar ayudar comer "
-    "esperar llamar comprar vender tomar hablar estudiar trabajar "
-    "empezar terminar jugar vivir correr caminar saltar cantar "
-    "bailar viajar visitar nadar manejar jugar practicar dormir "
-    "enseñar aprender leer escribir entender querer gustar "
-    "necesitar saber poder hablar escuchar dar conocer creer "
-    "preguntar contestar pensar sentir ver buscar encontrar "
-    "trabajar comprar vender pedir esperar llamar estar hacer "
-    "jugar caminar correr estudiar comer beber vivir viajar "
-    "enseñar aprender hablar escuchar entender querer gustar "
-    "necesitar saber poder escribir cantar bailar nadar manejar "
-    "trabajar comprar vender pedir comer correr estudiar "
-    "jugar aprender enseñar vivir gustar necesitar saber "
-    "hablar escuchar entender querer viajar bailar cantar "
-    "enseñar escribir leer ver estudiar comer trabajar "
-    "nadar hablar dormir escuchar correr empezar terminar "
-    "subir bajar abrir cerrar buscar encontrar vivir "
-    "morir matar gustar parecer costar llevar traer "
-    "pensar sentir hacer conocer dejar amar llamar"
-)
+openai.api_type = "azure"
+openai.api_version = "2023-05-15"
 
 listeninghearing = ("Type what you hear")
+speakthissentence = ("Speak this sentence")
 writeinspanish = ("Write this in Spanish")
-fillintheblanks = ("Fill In The blank")
+fillintheblanks = ("Fill in the blank")
 writeinenglish = ("Write this in English")
 howtosaykeys = ("How do you say")
 whichonekeys = ("Which one of these is")
@@ -115,7 +60,7 @@ def mainscript():
                     typeinspanish()
                 if fillintheblanks in result:
                     print("found phrase: fillintheblanks: ", fillintheblanks)
-                    fillintheblanks()
+                    fillintheblanksmethod()
                 if writeinenglish in result:
                     print("found phrase: writeinenglish: ", writeinenglish)
                     typeinenglish()
@@ -125,6 +70,9 @@ def mainscript():
                 if whichonekeys in result:
                     print("found phrase: whichone ", whichonekeys)
                     whichone()
+                if speakthissentence in result:
+                    print("found phrase: speakthissentence ", speakthissentence)
+                    listenandhear(579, 1007, 1333, 1002)
                 else:
                     print("could not find word")
                     index += 1
