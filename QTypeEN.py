25fb9f78461816565557bfbb9d59b75b5b23d8d7
Safir Azam <azamsafir@gmail.com>
2023-08-22 16:42:26 -0700

    began work on some issues with contractions

--- QTypeEN.py
+++ QTypeEN.py
@@ -9,6 +9,89 @@ checkbutton = 1340, 1131
 reader = easyocr.Reader(["en", "es"])
 mon = {'left': 752, 'top': 403, 'width': 590, 'height': 170}
 
+contraction_dict = {
+    "ain't": "is not",
+    "amn't": "am not",
+    "aren't": "are not",
+    "can't": "cannot",
+    "could've": "could have",
+    "couldn't": "could not",
+    "didn't": "did not",
+    "doesn't": "does not",
+    "don't": "do not",
+    "gonna": "going to",
+    "gotta": "got to",
+    "hadn't": "had not",
+    "hasn't": "has not",
+    "haven't": "have not",
+    "he'd": "he would",
+    "he'll": "he will",
+    "he's": "he is",
+    "how'd": "how did",
+    "how'll": "how will",
+    "how's": "how is",
+    "I'd": "I would",
+    "I'll": "I will",
+    "I'm": "I am",
+    "I've": "I have",
+    "isn't": "is not",
+    "it'd": "it would",
+    "it'll": "it will",
+    "it's": "it is",
+    "let's": "let us",
+    "ma'am": "madam",
+    "might've": "might have",
+    "mustn't": "must not",
+    "must've": "must have",
+    "needn't": "need not",
+    "oughtn't": "ought not",
+    "shan't": "shall not",
+    "she'd": "she would",
+    "she'll": "she will",
+    "she's": "she is",
+    "should've": "should have",
+    "shouldn't": "should not",
+    "somebody's": "somebody is",
+    "someone's": "someone is",
+    "something's": "something is",
+    "that'd": "that would",
+    "that's": "that is",
+    "there'd": "there would",
+    "there's": "there is",
+    "they'd": "they would",
+    "they'll": "they will",
+    "they're": "they are",
+    "they've": "they have",
+    "wasn't": "was not",
+    "we'd": "we would",
+    "we'll": "we will",
+    "we're": "we are",
+    "we've": "we have",
+    "weren't": "were not",
+    "what'd": "what did",
+    "what's": "what is",
+    "when'd": "when did",
+    "when'll": "when will",
+    "when's": "when is",
+    "where'd": "where did",
+    "where's": "where is",
+    "who'd": "who would",
+    "who'll": "who will",
+    "who's": "who is",
+    "why'd": "why did",
+    "why'll": "why will",
+    "why's": "why is",
+    "won't": "will not",
+    "would've": "would have",
+    "wouldn't": "would not",
+    "y'all": "you all",
+    "you'd": "you would",
+    "you'll": "you will",
+    "you're": "you are",
+    "you've": "you have"
+}
+
+contractions_upper = ' '.join([word.capitalize() for word in contraction_dict.strip().split()])
 
 def typeinenglish():
     with mss.mss() as sct:
@@ -29,11 +112,6 @@ def typeinenglish():
             translatedtext = response_json['responseData']['translatedText']
             print(url)
             print(translatedtext)
-
-            pyautogui.write(translatedtext, interval=0.1)
-            pyautogui.moveTo(checkbutton, duration=1)
-            pyautogui.leftClick()
-            pyautogui.leftClick()
 
             time.sleep(0.2)
             from main import mainscript
