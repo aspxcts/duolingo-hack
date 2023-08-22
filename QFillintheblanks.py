5350c5c8b88e4a85cda00f812e125e82d0f960ae
Safir Azam <azamsafir@gmail.com>
2023-08-22 12:26:39 -0700

    bug fixes

--- QFillintheblanks.py
+++ QFillintheblanks.py
@@ -14,38 +14,24 @@ openai.api_base = endpoint
 openai.api_key = key
 
 sentences = []
-lastwordprompt = '''Which one of these sentences makes the most sense? Please respond in the following format:
+prompt = '''Which one of these sentences makes the most sense? Please respond in the following format:
 Sentence number of the sentence that makes the most sense, starting from 1 (if two or more sentences make somewhat sense, pick the one that makes the most sense)
 your answer should only contain a number
 could you respond with nothing, not even the sentences, just the format
 Here are your sentences:  
 '''
-middletwowordprompt = '''
-Which one of these sentences makes the most sense? Please respond in the following format:
-Sentence number of the sentence that makes the most sense, starting from 1 (if two or more sentences make somewhat sense, pick the one that makes the most sense)
-your answer should only contain a number
-could you respond with nothing, not even the sentences, just the format
-Here are your sentences:  
-'''
-lastwordprompt = str(lastwordprompt)
-middletwowordprompt = str(lastwordprompt)
-
-checkbutton = 1340, 1131
+prompt = str(prompt)
 
 reader = easyocr.Reader(["en", "es"])
-mon = {'left': 630, 'top': 433, 'width': 600, 'height': 150}
-mon2 = {'left': 781, 'top': 546, 'width': 300, 'height': 500}
-
-
-def cycle_through_lists(sentence, options):
-    result1 = sentence[0] + ' ' + options[0] + ' ' + sentence[1] + ' ' + options[1] + ' ' + sentence[2]
-    result2 = sentence[0] + ' ' + options[1] + ' ' + sentence[1] + ' ' + options[0] + ' ' + sentence[2]
-    return result1, result2
+mon = {'top': 370, 'left': 629, 'width': 600, 'height': 150}
 
 
 def single_last_word():
     with mss.mss() as sct:
+        mon2 = {'top': 500, 'left': 854, 'width': 300, 'height': 500}
         while True:
+            sep = ''
+            sepspace = ' '
             im = numpy.asarray(sct.grab(mon))
             sentence = reader.readtext(im, detail=0)
             sep = ''
@@ -58,7 +44,7 @@ def single_last_word():
             print(options)
             stringoptions = sepspace.join(options)
             print(stringoptions)
-            mainprompt = lastwordprompt
+            mainprompt = prompt
             for option in options:
                 fullsentence = ''
                 fullsentence += stringsentence
@@ -72,22 +58,22 @@ def single_last_word():
                 engine="gpt-35-turbo",
                 messages=[
                     {"role": "user", "content": mainprompt}
-                ]
-            )
+            ]
+        )
             print(sentencecheck['choices'][0]['message']['content'])
             sentencecheck = str(sentencecheck['choices'][0]['message']['content'])
 
             pyautogui.write(sentencecheck)
-            pyautogui.moveTo(checkbutton, duration=1)
+            pyautogui.moveTo(1334, 1003, duration=1)
             pyautogui.leftClick()
             pyautogui.leftClick()
 
             from main import mainscript
             mainscript()
 
-
-def double_middle_word():
+def single_middle_word():
     with mss.mss() as sct:
+        mon2 = {'top': 500, 'left': 854, 'width': 300, 'height': 500}
         while True:
             sep = ''
             sepspace = ' '
@@ -99,43 +85,31 @@ def double_middle_word():
             stringsentence = sep.join(sentence)
             print(stringsentence)
             im2 = numpy.asarray(sct.grab(mon2))
-            optionables = reader.readtext(im2, detail=0)
-            print(optionables)
-            stringoptions = sepspace.join(optionables)
+            options = reader.readtext(im2, detail=0)
+            print(options)
+            stringoptions = sepspace.join(options)
             print(stringoptions)
-            mainprompt = middletwowordprompt
-
-            result1, result2 = cycle_through_lists(sentence, optionables)
-            mainprompt += result1
-            mainprompt += '\n'
-            mainprompt += result2
-            print(mainprompt)
+            mainprompt = prompt
+            for option in options:
+                fullsentence = ''
+                fullsentence += stringsentence
+                fullsentence += ' '
+                fullsentence += option
+                mainprompt += fullsentence
+                mainprompt += "\n"
+                print(mainprompt)
 
             sentencecheck = openai.ChatCompletion.create(
                 engine="gpt-35-turbo",
                 messages=[
                     {"role": "user", "content": mainprompt}
-                ]
-            )
+            ]
+        )
             print(sentencecheck['choices'][0]['message']['content'])
-            sentencecheck = int(sentencecheck['choices'][0]['message']['content'])
+            sentencecheck = str(sentencecheck['choices'][0]['message']['content'])
 
-            if sentencecheck == 1:
-                writeable = optionables[0]
-                print(writeable)
-                pyautogui.write(writeable)
-                writeable = optionables[1]
-                print(writeable)
-                pyautogui.write(writeable)
-            if sentencecheck == 2:
-                writeable = optionables[1]
-                print(writeable)
-                pyautogui.write(writeable)
-                writeable = optionables[0]
-                print(writeable)
-                pyautogui.write(writeable)
-
-            pyautogui.moveTo(checkbutton, duration=1)
+            pyautogui.write(sentencecheck)
+            pyautogui.moveTo(1334, 1003, duration=1)
             pyautogui.leftClick()
             pyautogui.leftClick()
 
@@ -145,6 +119,7 @@ def double_middle_word():
 
 def createsentence():
     with mss.mss() as sct:
+        mon2 = {'top': 500, 'left': 854, 'width': 300, 'height': 500}
         while True:
             url = "https://api.mymemory.translated.net/get?q="
             sep = ''
@@ -155,7 +130,7 @@ def createsentence():
             if len(sentence) == 0:
                 single_last_word()
             if len(sentence) > 0:
-                double_middle_word()
+                single_middle_word()
 
             # texttotranslate = sep.join(readerresult)
 
